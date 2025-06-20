96f8911f3e64a8e7bd2d78baf2551ef3a2fbfd9d
Moh Quthbul Widad <jeffhaplasca@gmail.com>
2025-06-20 15:18:24 +0700

    push github

--- config/services.php
+++ config/services.php
@@ -6,12 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
     */
 
     'postmark' => [
@@ -33,6 +27,11 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ], // <-- Perhatikan, array 'slack' berakhir di sini
+
+    // 'newsdata' berada di luarnya, sejajar dengan yang lain
+    'newsdata' => [
+        'key' => env('NEWSDATA_API_KEY'),
     ],
 
 ];
