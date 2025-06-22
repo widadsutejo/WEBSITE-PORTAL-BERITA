5691f5a73b5eb7d99c7dba5c5bd874055cf804e5
Moh Quthbul Widad <jeffhaplasca@gmail.com>
2025-06-22 19:54:27 +0700

    Add custom health check route for debugging

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\LandingController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
+// Tambahkan route ini di paling atas
+Route::get('/health', function () {
+    return response('OK', 200);
+});
+
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 Route::get('/{slug}', [NewsController::class, 'category'])->name('news.category');
